ff479e9884c4b9f9b40d18d4e149eeabd21b2b9f
l3miage-mokniy <yanis.mokni@gmail.com>
2021-04-12 14:57:24 +0200

    UPDATE environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyBg7qWBRAKHEwc80OoMqwTKZMers-9lxXk",
+  authDomain: "pi-g8-miage.firebaseapp.com",
+  projectId: "pi-g8-miage",
+  storageBucket: "pi-g8-miage.appspot.com",
+  messagingSenderId: "167070504543",
+  appId: "1:167070504543:web:4a085adc58045285ae2dea"}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase : {
+  firebase: {
   apiKey: "AIzaSyBg7qWBRAKHEwc80OoMqwTKZMers-9lxXk",
   authDomain: "pi-g8-miage.firebaseapp.com",
   projectId: "pi-g8-miage",
