e8c60a19d74ce8f2d5d4712af1a66a263c4c068c
demeurea <alexandre.demeure@univ-grenoble-alpes.fr>
2021-04-12 09:18:05 +0200

    with yaga map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { OSM_TILE_LAYER_URL } from '@yaga/leaflet-ng2';
 })
 export class AppComponent {
   dataIconGoogle = 'assets/images/iconGoogle.png';
-
+  iconMarker = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/585px-Map_marker.svg.png';
+  tileLayerUrl = OSM_TILE_LAYER_URL;
 }
