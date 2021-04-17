196d2af34a6e5b3e6b86c98844dab5f069b992de
l3miage-mokniy <yanis.mokni@gmail.com>
2021-04-17 22:16:38 +0200

    MARQUEUR ARRET DYNAMIQUE FONCTIONELLE

--- src/app/map.service.ts
+++ src/app/map.service.ts
@@ -1,96 +1,73 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Arret, rgbToHex, ArretMap} from './AllDefinitions';
+import { Subject } from 'rxjs';
+import { Arret, rgbToHex, ArretMap } from './AllDefinitions';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MapService {
-
-  private arretsSubj = new BehaviorSubject<ArretMap[]>( [] );
+  private arretsSubj = new Subject<ArretMap[]>();
   readonly arretsObs = this.arretsSubj.asObservable();
 
-
-  private lignesSubj = new BehaviorSubject<GeoJSON.Feature<GeoJSON.LineString | GeoJSON.MultiLineString,any>[]>( [] );
+  private lignesSubj = new Subject<GeoJSON.Feature<GeoJSON.LineString | GeoJSON.MultiLineString, any>[]>();
   readonly lignesObs = this.lignesSubj.asObservable();
-
-  /*
-  private arretsSubj = new BehaviorSubject<GeoJSON.Feature<GeoJSON.Point,any>[]>( [] );
-  readonly arretsObs = this.arretsSubj.asObservable();
-*/
 
   lignes: GeoJSON.Feature<GeoJSON.LineString | GeoJSON.MultiLineString,any>[] = [];
   arrets: ArretMap[] = [];
 
-constructor() {
-  //this.recupArretAvecDefiAPIPerso()
-  this.recupArretAvecDefiAPIPerso()
-  this.recupAllLinesSEMITAG()
-}
+  constructor() {
+    this.recupAllLinesSEMITAG();
+    this.recupArretAvecDefiAPIPerso();
+  }
 
-///////////////////////////////////////////ARRET///////////////////////////////////////////
-
-
-async recupArretAvecDefiAPIPerso() {
-
-  const response = await fetch('https://l3m-pi-serveur-g8.herokuapp.com/api/arret/defi')
-  const dataAPISpringBoot = await response.json();
-
-  const tabOfAllArret:Arret[] = dataAPISpringBoot as Arret[]
-  tabOfAllArret.map(
-    x => {
-       this.recupArretAvecDefiAPIMobi(x.lib_arret)
-    })
-    console.log("avant next"+this.arrets)
-    this.arretsSubj.next(this.arrets)
-}
-
-async recupArretAvecDefiAPIMobi(lib: string) {
-  const response = await fetch(
-    'https://data.mobilites-m.fr/api/findType/json?types=arret&query='+lib
+  ///////////////////////////////////////////ARRET///////////////////////////////////////////
+  async recupArretAvecDefiAPIPerso() {
+    const response = await fetch('https://l3m-pi-serveur-g8.herokuapp.com/api/arret/defi');
+    const dataAPISpringBoot = await response.json();
+    const tabOfAllArret: Arret[] = dataAPISpringBoot as Arret[];
+    const L = await Promise.all(
+      tabOfAllArret.map(x =>this.recupArretAvecDefiAPIMobi(x.lib_arret))
     );
-  const data = await response.json();
-  const test = JSON.stringify(data);
-  const test20 = JSON.parse(test);
-  ////FIN RECUPERATION DU POINT SUR API MOBILITE////
-  console.log(data)
+    this.arretsSubj.next(L)
+    this.arrets = L;
+  }
 
-  //console.log("arret :::::::::::: "+test20.features[0].properties.CODE)
-  const responseAPIPerso = await fetch('https://l3m-pi-serveur-g8.herokuapp.com/api/arret/nb_defi/'+test20.features[0].properties.CODE);
-  let nombre_defi = await responseAPIPerso.text()
-  //console.log("nb defi "+ nombre_defi)
+  async recupArretAvecDefiAPIMobi(lib: string) : Promise<ArretMap>{
+    const response = await fetch('https://data.mobilites-m.fr/api/findType/json?types=arret&query=' + lib);
+    const data = await response.json();
+    const dataString = JSON.stringify(data);
+    const dataJSON = await JSON.parse(dataString);
+    ////FIN RECUPERATION DU POINT SUR API MOBILITE////
 
-  ///FIN RECUPERATION DU NB DE DEFI POUR UN ARRET///
+    const responseAPIPerso = await fetch('https://l3m-pi-serveur-g8.herokuapp.com/api/arret/nb_defi/' + dataJSON.features[0].properties.CODE);
+    let nombre_defi = await responseAPIPerso.text();
+    ///FIN RECUPERATION DU NB DE DEFI POUR UN ARRET///
 
-  this.arrets.push({
-    nb_defi : +nombre_defi,
-    info_arret :test20.features[0]
-  })
-}
+    return {
+      nb_defi: +nombre_defi,
+      info_arret: dataJSON.features[0]
+    }
+  }
 
-displayDefi(i: number) {
-  console.log("METHODE DISPLAY DEFI "+this.arrets[i].info_arret.properties.CODE)
-}
+  displayDefi(i: number) {
+    console.log(
+      'METHODE DISPLAY DEFI ' + this.arrets[i].info_arret.properties.CODE
+    );
+  }
 
+  ///////////////////////////////////////////LIGNE///////////////////////////////////////////
+  async recupAllLinesSEMITAG() {
+    const response = await fetch(
+      'https://data.mobilites-m.fr/api/lines/json?types=ligne&reseaux=SEM'
+    );
+    const data = await response.json();
+    const test = JSON.stringify(data);
+    const test20 = JSON.parse(test);
+    this.lignes = test20.features;
+    this.lignesSubj.next(this.lignes);
+  }
 
-
-///////////////////////////////////////////LIGNE///////////////////////////////////////////
-async recupAllLinesSEMITAG() {
-  const response = await fetch(
-    'https://data.mobilites-m.fr/api/lines/json?types=ligne&reseaux=SEM'
-  );
-  const data = await response.json();
-  const test = JSON.stringify(data);
-  const test20 = JSON.parse(test);
-  this.lignes = test20.features;
-  this.lignesSubj.next(this.lignes);
-
-}
-
-colorationLines(i: number): string | undefined {
-  return rgbToHex(this.lignes[i].properties.COULEUR);
-}
-
-
-
+  colorationLines(i: number): string | undefined {
+    return rgbToHex(this.lignes[i].properties.COULEUR);
+  }
 }
